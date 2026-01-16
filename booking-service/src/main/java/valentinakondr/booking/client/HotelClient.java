8b26535f3df926df86b2b6a0fb72cbbbaa75ca5b
Kondrashina Valentina <Vkondr04@icloud.com>
2026-01-16 16:30:00 +0300

    small edits 2

--- booking-service/src/main/java/valentinakondr/booking/client/HotelClient.java
+++ booking-service/src/main/java/valentinakondr/booking/client/HotelClient.java
@@ -1,6 +1,5 @@
 package valentinakondr.booking.client;
 
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 import valentinakondr.booking.config.feign.FeignAuthRequestInterceptor;
